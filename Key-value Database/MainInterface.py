0e9440e18d636aaef97c8ab75dd8273e0240a6ab
Farhan Khan <97287314+farnsolo@users.noreply.github.com>
2024-03-30 18:08:53 +0000

    MainInterface.py upload
    MainInterface file contains two methods 
    get and setName (This name is subject to change)
    Both these methods are interacted with in the network file>
    
    get retrieves a value for a given key 
    setName creates new Key-value pair for given server name and ip

---
+++ Key-value Database/MainInterface.py
@@ -0,0 +1,9 @@
+from Backend import Dict
+def getName(name):
+    # Return name from given IP
+    return Dict[name]
+    
+def setName(key, value):
+    # set name for ip
+    # create new key-value pair
+    Dict[key] = value
