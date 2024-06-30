4071a9b6c983eccf53ba41efadbb1da086191c39
Farhan Khan <97287314+farnsolo@users.noreply.github.com>
2024-06-30 21:01:00 +0100

    Local network update
    This verison of serverCPC.py is only compatible within a private network
    See ReadMe for private/local network

--- serverCPC.py
+++ serverCPC.py
@@ -1,17 +1,22 @@
 import socket
 from tkinter import messagebox
 import threading
+import backendInterface
 
 class serverC:
-    def __init__(self):
-        self.users = []
+    def __init__(self, name):
+        self.usersIP = []
+        self.userNames = {}
         # socket.create_server could come in use
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.HOST = 'xxxxxxxxxx'
+        hostName = socket.gethostname()
+        self.HOST = socket.gethostbyname(hostName)
         self.PORT = 2525
+        self.NAME = name
+        backendInterface.listServer(self.NAME,self.HOST)
         self.sock.bind((self.HOST, self.PORT))
         self.sock.listen()
-        print(f"Server listening on {self.HOST}:{self.PORT}")
+        messagebox.showinfo("",f"Server listening on {self.HOST}:{self.PORT}")
         
     def create_server(self):
         self.server_running = True  
@@ -19,7 +24,7 @@ class serverC:
             try:
                 # Accept connection
                 self.conn, self.addr = self.sock.accept()
-                self.users.append(self.conn)
+                self.usersIP.append(self.conn)
                 self.broadcast(f"User {self.addr} has connected")
                 self.t1 = threading.Thread(target=self.receive, args=(self.conn,self.addr))
                 self.t1.daemon = True
@@ -29,14 +34,19 @@ class serverC:
                     messagebox.showinfo("Server Notification", "Server has Stopped")
                     break
                 else:
-                    raise e
+                    messagebox.showerror(e)
     
     def receive(self, conn, addr):
+        # byteString time!
         while self.server_running:
                     try:
-                        message = conn.recv(1024).decode()
-                        if message:
-                            self.broadcast(f"User {addr} says: " + message)
+                        message = conn.recv(1024)
+                        commandRecv = message[:1].decode()
+                        messageRecv = message[1:len(message)].decode()
+                        if commandRecv == "1":
+                            self.broadcast(f"User {self.userNames[addr[0]]} says: " + messageRecv)
+                        elif commandRecv == "0":
+                            self.userNames[addr[0]] = messageRecv
                         else:
                             self.delete_user(self.conn)
                     except OSError as e:
@@ -47,21 +57,22 @@ class serverC:
         
             
     def broadcast(self, message):
-        for user in self.users:
+        for user in self.usersIP:
             user.sendall(message.encode())
             
     def delete_user(self, conn):
-        for user in self.users:
+        for user in self.usersIP:
             if user == conn:
-                self.users.remove(user)
+                self.usersIP.remove(user)
                 self.conn.shutdown(socket.SHUT_RDWR)
                 self.conn.close()
 
 
     def stop_server(self):
         self.server_running = False
-        for user in self.users:
+        for user in self.usersIP:
             # fin ack shutdown with each user 
             user.shutdown(socket.SHUT_RDWR)
             user.close()
         self.sock.close()
+        del self.sock
