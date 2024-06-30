f63a3a7d128b867cd59a686d3877f0f461cc7c11
Farhan Khan <97287314+farnsolo@users.noreply.github.com>
2024-06-30 21:04:10 +0100

    Local network update
    This verison of Network.py is only compatible within a private network
    See ReadMe for private/local network

--- Key-value Database/Network.py
+++ Key-value Database/Network.py
@@ -1,39 +1,40 @@
 import socket
-from MainInterface import getName
-from MainInterface import setName
-import time
+import pickle
+from MainInterface import getIP, createPair, removeServer, getServerNames
 
 # handle communication to server, use just TCP
-# May use dnsPython but want to see if this will surfice
+# May use dnsPython but want to see if this will suffice
+
+# Make own DNS thing
+# Binary encoded
+# First three bits for operation + message encoded in ASCII
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(("192.168.1.107", 1010))
+sock.bind(("192.168.1.127", 1010))
 sock.listen()
 
 while(True):
-    try:
         conn, addr = sock.accept()
-        while True:
-            try:
-                message = conn.recv(1024).decode()
-                # FIND ALTERNATIVE TO 1
-                if message == "1":
-                    message = conn.recv(1024).decode()
-                    conn.sendall(getName(message).encode())
-                elif message == "2":
-                    i = 0
-                    messageBlock = []
-                    while (i < 2):
-                        # Remove try-catch perhaps
-                        try:
-                            message = conn.recv(1024).decode()
-                            messageBlock.append(message)
-                            i+=1
-                        except Exception as e:
-                            print(e)
-                    setName(messageBlock[0],messageBlock[1])               
-            except Exception as e:
-                print(e)
-                break
-    except:
-        print("Error")
+        try:
+            print(addr)
+            message = conn.recv(1024)
+            length = 0
+            length = message[:1].decode() if message[1:2].decode() == "-" else message[:2].decode()
+            length  = int(length)
+            length += 3
+            commandRecv = message[2:3].decode()
+            serverNameRecv = message[3:length]
+            ipRecv = message[length:len(message)]
+            
+            if commandRecv == "1":
+                ip = getIP(serverNameRecv)
+                conn.sendall(ip)
+            elif  commandRecv == "4":
+                data = pickle.dumps(getServerNames())                     
+                conn.sendall(data)
+            elif commandRecv == "2":
+                createPair(serverNameRecv,ipRecv)
+            else:
+                removeServer(serverNameRecv)
+        except IOError as exception:
+            print(exception)
