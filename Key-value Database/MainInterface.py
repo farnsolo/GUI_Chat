c6b47101f8da81b647dc9edac3ce9144192f0098
Farhan Khan <97287314+farnsolo@users.noreply.github.com>
2024-06-30 21:04:52 +0100

    Local network update
    This verison of clientCPC.py is only compatible within a private network
    See ReadMe for private/local network

--- Key-value Database/MainInterface.py
+++ Key-value Database/MainInterface.py
@@ -1,9 +1,16 @@
 from Backend import Dict
-def getName(name):
+def getIP(name):
     # Return name from given IP
     return Dict[name]
     
-def setName(key, value):
+def createPair(key, value):
     # set name for ip
     # create new key-value pair
     Dict[key] = value
+
+def getServerNames():
+    return list(Dict.keys())
+
+# Time complexity O(1)
+def removeServer(key):
+    del Dict[key]
